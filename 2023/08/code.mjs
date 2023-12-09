347c19061231020f761c270a7e6cfe57e50d05ce
plasmapper <99073058+plasmapper@users.noreply.github.com>
2023-12-09 18:39:50 +0100

    Removed unused constant from 2023 day 08

--- 2023/08/code.mjs
+++ 2023/08/code.mjs
@@ -1,7 +1,5 @@
 import { delay, Console, leastCommonMultiple } from "../../utility.mjs";
 
-let cardValues = {2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, T: 10, J: 11, Q: 12, K: 13, A: 14};
-
 export default class  {
   /**
    * @param {Console} solConsole Solution console.
