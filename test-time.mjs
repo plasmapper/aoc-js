94c653e08d21fa3f372dfca6c3ecbd083a8d9b13
plasmapper <99073058+plasmapper@users.noreply.github.com>
2025-01-15 10:11:52 +0100

    Updated time test

--- test-time.mjs
+++ test-time.mjs
@@ -1,10 +1,10 @@
 import fs from "fs";
 import {years} from "./tree.mjs";
 
-let x0 = 6, dx = 10, y0 = 1;
+let x0 = 7, dx = 6, y0 = 1;
 let x = x0, y = y0;
 
-console.log(" ".repeat(x0 + 6) + years.map(year => `${year.name}`).join("      "));
+console.log(" ".repeat(x0 + 1) + years.map(year => `${year.name}`).join("  "));
 for (let i = 1; i <= 25; i++)
   console.log(`Day ${i < 10 ? " " : ""}${i}`);
 process.stdout.cursorTo(x0);
@@ -18,9 +18,9 @@ for (let year of years) {
       
       let start = performance.now();
       await code.solve(1, input, false);
-      let part1Time = (Math.ceil(performance.now() - start)).toString().padStart(4);
+      //let part1Time = (Math.ceil(performance.now() - start)).toString().padStart(4);
       
-      start = performance.now();
+      //start = performance.now();
       let part2Time = 0;
       if (code.noPart2 != true) {
         await code.solve(2, input, false);
@@ -28,10 +28,10 @@ for (let year of years) {
       }
       part2Time = part2Time.toString().padStart(4);
 
-      part1Time = `${part1Time > 5000 ? "\x1b[31m" : (part1Time > 1000 ? "\x1b[33m" : "\x1b[32m")}${part1Time}\x1b[0m`;
-      part2Time = `${part2Time > 5000 ? "\x1b[31m" : (part2Time > 1000 ? "\x1b[33m" : "\x1b[32m")}${part2Time}\x1b[0m`;
+      let time = Math.ceil(performance.now() - start);
+      let timeString = `${time > 5000 ? "\x1b[31m" : (time > 1000 ? "\x1b[33m" : "\x1b[32m")}${time.toString().padStart(4)}\x1b[0m`;
 
-      process.stdout.write(` ${part1Time}/${part2Time}`);
+      process.stdout.write(` ${timeString}`);
       process.stdout.cursorTo(x);
       process.stdout.moveCursor(0, 1);
       y++;
