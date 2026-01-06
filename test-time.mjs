8e7af30d04c11a34bc9288664751c8a5bd2da3ee
plasmapper <99073058+plasmapper@users.noreply.github.com>
2026-01-06 18:15:37 +0100

    Added year argument to test-time script

--- test-time.mjs
+++ test-time.mjs
@@ -11,27 +11,29 @@ process.stdout.cursorTo(x0);
 process.stdout.moveCursor(0, -25);
 
 for (let year of years) {
-  for (let day of year.days) {
-    if (day.disabled != true) {
-      let code = new (await import(day.path + "/code.mjs")).default();
-      let input = (await fs.promises.readFile(day.path + "/puzzleInput.txt")).toString();
-      
-      let timeString = "";
-      if (!code.isMD5Calculation) {
-        let start = performance.now();
-        await code.solve(1, input, false);
-        if (code.noPart2 != true)
-          await code.solve(2, input, false);
-        let time = Math.ceil(performance.now() - start);
-        timeString = `${time > 5000 ? "\x1b[31m" : (time > 1000 ? "\x1b[33m" : "\x1b[32m")}${time.toString().padStart(4)}\x1b[0m`;
-      }
-      else
-        timeString = " MD5";
+  if (process.argv[2] == undefined || year.name == process.argv[2]) {
+    for (let day of year.days) {
+      if (day.disabled != true) {
+        let code = new (await import(day.path + "/code.mjs")).default();
+        let input = (await fs.promises.readFile(day.path + "/puzzleInput.txt")).toString();
+        
+        let timeString = "";
+        if (!code.isMD5Calculation) {
+          let start = performance.now();
+          await code.solve(1, input, false);
+          if (code.noPart2 != true)
+            await code.solve(2, input, false);
+          let time = Math.ceil(performance.now() - start);
+          timeString = `${time > 5000 ? "\x1b[31m" : (time > 1000 ? "\x1b[33m" : "\x1b[32m")}${time.toString().padStart(4)}\x1b[0m`;
+        }
+        else
+          timeString = " MD5";
 
-      process.stdout.write(` ${timeString}`);
-      process.stdout.cursorTo(x);
-      process.stdout.moveCursor(0, 1);
-      y++;
+        process.stdout.write(` ${timeString}`);
+        process.stdout.cursorTo(x);
+        process.stdout.moveCursor(0, 1);
+        y++;
+      }
     }
   }
 
